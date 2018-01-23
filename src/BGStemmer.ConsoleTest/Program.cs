14e7b2ff83fb629a7386da7cdcced70ec58e5df7
angelinn <angelin.nedelchev@outlook.com>
2018-01-23 22:49:13 +0200

    Add console test and rule files.

---
+++ src/BGStemmer.ConsoleTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGStemmer.ConsoleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BulgarianStemmer stemmer = new BulgarianStemmer();
+            stemmer.LoadRules("../../stem_rules_context_3.txt");
+
+            Console.WriteLine(stemmer.Stem("каране"));
+        }
+    }
+}
