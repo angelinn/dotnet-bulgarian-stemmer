3b4a6f0e9ee6ccfc0f44ce773e0aa7bf0ec2949a
angelinn <angelin.nedelchev@outlook.com>
2018-01-23 23:04:57 +0200

    Add RulesException.

---
+++ src/BGStemmer/Exceptions/RulesException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BGStemmer.Exceptions
+{
+    public class RulesException : Exception
+    {
+        public RulesException(string message = "") : base(message)
+        {   }
+    }
+}
