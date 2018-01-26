1e03f47a27b8431183fde5e0db48d00e9234b1b9
angelinn <angelin.nedelchev@outlook.com>
2018-01-26 21:24:14 +0200

    Implement interface for lucene token filter.

---
+++ src/BGStemmer.Lucene/AdvancedBulgarianStemFilter.cs
@@ -0,0 +1,45 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.TokenAttributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BGStemmer.Lucene
+{
+    public class AdvancedBulgarianStemFilter : TokenFilter
+    {
+        private readonly BulgarianStemmer stemmer = new BulgarianStemmer();
+        private readonly ICharTermAttribute termAtt;
+        private readonly IKeywordAttribute keywordAttr;
+
+        public AdvancedBulgarianStemFilter(TokenStream input)
+              : base(input)
+        {
+            termAtt = AddAttribute<ICharTermAttribute>();
+            keywordAttr = AddAttribute<IKeywordAttribute>();
+        }
+
+        public override bool IncrementToken()
+        {
+            if (m_input.IncrementToken())
+            {
+                if (!keywordAttr.IsKeyword)
+                {
+                    string stemmed = stemmer.Stem(termAtt.Buffer.ToString());
+                    if (stemmed.Length > termAtt.Length)
+                        throw new Exception("wtf");
+
+                    for (int i = 0; i < stemmed.Length; ++i)
+                        termAtt[i] = stemmed[i];
+
+                    termAtt.Length = stemmed.Length;
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/BGStemmer.Lucene/AdvancedBulgarianStemFilterFactory.cs
@@ -0,0 +1,25 @@
+﻿using Lucene.Net.Analysis.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Lucene.Net.Analysis;
+
+namespace BGStemmer.Lucene
+{
+    public class AdvancedBulgarianStemFilterFactory : TokenFilterFactory
+    {
+        public AdvancedBulgarianStemFilterFactory(IDictionary<string, string> args)
+              : base(args)
+        {
+            if (args.Count > 0)
+            {
+                throw new ArgumentException("Unknown parameters: " + args);
+            }
+        }
+
+        public override TokenStream Create(TokenStream input)
+        {
+            return new AdvancedBulgarianStemFilter(input);
+        }
+    }
+}
